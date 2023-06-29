6ecb47c3477ec45a957eb3af578fe2e1cf6d097b
FranekStratovarius <louishuss2000@gmail.com>
2023-06-29 13:05:12 +0200

    changd data types

--- database/init.sql
+++ database/init.sql
@@ -1,21 +1,21 @@
 CREATE TABLE ragequits (
 	id			SERIAL PRIMARY KEY NOT NULL,
 	level		INT NOT NULL,
-	time		INT NOT NULL,
-	positionx	INT NOT NULL,
-	positiony	INT NOT NULL
+	time		DECIMAL NOT NULL,
+	positionx	DECIMAL NOT NULL,
+	positiony	DECIMAL NOT NULL
 );
 
 CREATE TABLE wins (
 	id			SERIAL PRIMARY KEY NOT NULL,
 	level		INT NOT NULL,
-	time		INT NOT NULL
+	time		DECIMAL NOT NULL
 );
 
 CREATE TABLE deaths (
 	id			SERIAL PRIMARY KEY NOT NULL,
 	level		INT NOT NULL,
-	time		INT NOT NULL,
-	positionx	INT NOT NULL,
-	positiony	INT NOT NULL
+	time		DECIMAL NOT NULL,
+	positionx	DECIMAL NOT NULL,
+	positiony	DECIMAL NOT NULL
 );
--- server/handlers.go
+++ server/handlers.go
@@ -11,23 +11,23 @@ import (
 type Ragequit struct {
 	ID        int64
 	Level     int64
-	Time      int64
-	PositionX int64
-	PositionY int64
+	Time      float64
+	PositionX float64
+	PositionY float64
 }
 
 type Win struct {
 	ID    int64
 	Level int64
-	Time  int64
+	Time  float64
 }
 
 type Death struct {
 	ID        int64
 	Level     int64
-	Time      int64
-	PositionX int64
-	PositionY int64
+	Time      float64
+	PositionX float64
+	PositionY float64
 }
 
 // upload handlers
@@ -40,7 +40,7 @@ func upload_ragequit(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		// fmt.Printf("time for ragequit: %d in level %d at Position %d,%d\n", ragequit.Time, ragequit.Level, ragequit.PositionX, ragequit.PositionY)
+		fmt.Printf("time for ragequit: %f in level %d at Position %f,%f\n", ragequit.Time, ragequit.Level, ragequit.PositionX, ragequit.PositionY)
 		_, err = db.Exec("INSERT INTO ragequits (level, time, positionx, positiony) VALUES ($1, $2, $3, $4)", ragequit.Level, ragequit.Time, ragequit.PositionX, ragequit.PositionY)
 		if err != nil {
 			log.Print("can't insert into ragequits db: ", err)
@@ -60,7 +60,7 @@ func upload_ragequit(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse ragequit from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d [%d|%d]\n", ctr, (ragequit.Time / 3600), (ragequit.Time/60)%60, ragequit.Time%60, ragequit.PositionX, ragequit.PositionY)
+			fmt.Fprintf(w, "#%d: %d:%d:%d [%f|%f]\n", ctr, int64(ragequit.Time/3600), int64(ragequit.Time/60)%60, int64(ragequit.Time)%60, ragequit.PositionX, ragequit.PositionY)
 		}
 		rows, err = db.Query("SELECT * FROM ragequits WHERE level = 2 ORDER BY time ASC LIMIT 10")
 		if err != nil {
@@ -76,7 +76,7 @@ func upload_ragequit(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse ragequit from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d [%d|%d]\n", ctr, (ragequit.Time / 3600), (ragequit.Time/60)%60, ragequit.Time%60, ragequit.PositionX, ragequit.PositionY)
+			fmt.Fprintf(w, "#%d: %d:%d:%d [%f|%f]\n", ctr, int64(ragequit.Time/3600), int64(ragequit.Time/60)%60, int64(ragequit.Time)%60, ragequit.PositionX, ragequit.PositionY)
 		}
 	default:
 		fmt.Fprintf(w, "Sorry, only POST and GET methods are supported.\n")
@@ -92,7 +92,7 @@ func upload_win(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		// fmt.Printf("time for win: %d in level %d\n", win.Time, win.Level)
+		fmt.Printf("time for win: %f in level %d\n", win.Time, win.Level)
 		_, err = db.Exec("INSERT INTO wins (level, time) VALUES ($1, $2)", win.Level, win.Time)
 		if err != nil {
 			log.Print("can't insert into wins db ", err)
@@ -112,7 +112,7 @@ func upload_win(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse win from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d\n", ctr, (win.Time / 3600), (win.Time/60)%60, win.Time%60)
+			fmt.Fprintf(w, "#%d: %d:%d:%d\n", ctr, int64(win.Time/3600), int64(win.Time/60)%60, int64(win.Time)%60)
 		}
 		rows, err = db.Query("SELECT * FROM wins WHERE level = 2 ORDER BY time ASC LIMIT 10")
 		if err != nil {
@@ -128,7 +128,7 @@ func upload_win(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse win from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d\n", ctr, (win.Time / 3600), (win.Time/60)%60, win.Time%60)
+			fmt.Fprintf(w, "#%d: %d:%d:%d\n", ctr, int64(win.Time/3600), int64(win.Time/60)%60, int64(win.Time)%60)
 		}
 	default:
 		fmt.Fprintf(w, "Sorry, only POST and GET methods are supported.\n")
@@ -144,7 +144,7 @@ func upload_death(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
-		// fmt.Printf("time for death: %d in level %d at Position %d,%d\n", death.Time, death.Level, death.PositionX, death.PositionY)
+		fmt.Printf("time for death: %f in level %d at Position %f,%f\n", death.Time, death.Level, death.PositionX, death.PositionY)
 		_, err = db.Exec("INSERT INTO deaths (level, time, positionx, positiony) VALUES ($1, $2, $3 ,$4)", death.Level, death.Time, death.PositionX, death.PositionY)
 		if err != nil {
 			log.Print("can't insert into deaths db ", err)
@@ -164,7 +164,7 @@ func upload_death(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse death from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d [%d|%d]\n", ctr, (death.Time / 3600), (death.Time/60)%60, death.Time%60, death.PositionX, death.PositionY)
+			fmt.Fprintf(w, "#%d: %d:%d:%d [%f|%f]\n", ctr, int64(death.Time/3600), int64(death.Time/60)%60, int64(death.Time)%60, death.PositionX, death.PositionY)
 		}
 		rows, err = db.Query("SELECT * FROM deaths WHERE level = 2 ORDER BY time ASC LIMIT 10")
 		if err != nil {
@@ -180,7 +180,7 @@ func upload_death(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 				log.Print("can't parse death from db: ", err)
 			}
 			ctr++
-			fmt.Fprintf(w, "#%d: %d:%d:%d [%d|%d]\n", ctr, (death.Time / 3600), (death.Time/60)%60, death.Time%60, death.PositionX, death.PositionY)
+			fmt.Fprintf(w, "#%d: %d:%d:%d [%f|%f]\n", ctr, int64(death.Time/3600), int64(death.Time/60)%60, int64(death.Time)%60, death.PositionX, death.PositionY)
 		}
 	default:
 		fmt.Fprintf(w, "Sorry, only POST and GET methods are supported.\n")
