dafd43d32f3ba72aa4b9be0b21443243416ffc8f
FranekStratovarius <louishuss2000@gmail.com>
2023-06-29 13:08:56 +0200

    added debug logs

--- server/main.go
+++ server/main.go
@@ -16,13 +16,27 @@ import (
 func main() {
 	var db = connect()
 
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("> /")
+		log.Print(r)
+	})
+
 	http.HandleFunc("/ragequit", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("> /ragequit")
+		log.Print(r)
+		log.Print(r.Body)
 		upload_ragequit(w, r, db)
 	})
 	http.HandleFunc("/win", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("> /win")
+		log.Print(r)
+		log.Print(r.Body)
 		upload_win(w, r, db)
 	})
 	http.HandleFunc("/death", func(w http.ResponseWriter, r *http.Request) {
+		log.Print("> /death")
+		log.Print(r)
+		log.Print(r.Body)
 		upload_death(w, r, db)
 	})
 
